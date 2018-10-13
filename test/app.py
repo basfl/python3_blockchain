ff27f230ef71f375357919802faba18602ee2346
basfl <jupiter85838@yahoo.com>
2018-10-13 00:08:32 -0400

    add test folder

---
+++ test/app.py
@@ -0,0 +1,11 @@
+import os.path
+import sys
+module_path="../"
+module_file=""
+path=os.path.join(module_path,module_file)
+abs_path=os.path.abspath(path)
+sys.path.append(abs_path)
+print(abs_path)
+from utility.printable import Printable
+pt=Printable()
+print(pt);
